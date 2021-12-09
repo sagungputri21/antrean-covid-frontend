dea7371144469cce566a5afde7c5cff6cf430d9b
sagungputri21 <93807864+sagungputri21@users.noreply.github.com>
2021-12-09 21:07:06 +0800

    adding title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <title>Vaksin Kuy</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
