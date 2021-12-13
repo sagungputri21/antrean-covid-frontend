ee9e0e02b03dcc3b18f474397a155e6f4c507b42
Gede Sudimahendra <93807864+sagungputri21@users.noreply.github.com>
2021-12-13 12:00:04 +0800

    Changing color in PasienNav

--- components/Sidebar/PasienNav.js
+++ components/Sidebar/PasienNav.js
@@ -6,7 +6,7 @@ const PasienNav = () => {
     return(
             <div className="w-64 h-full mt-10 ml-11 border-solid border border-purple-300 bg-white rounded-xl">
                 <div className="flex row border-b-2 mt-6">
-                    <img src="/bars.svg" alt="home icon" className="h-8 mr-4 ml-8 mb-6 hover:stroke-white" />
+                    <img src="/bars.svg" alt="home icon" className="h-8 mr-4 ml-8 mb-6 hover:fill-white"/>
                     <h2 className="text-purple-700 font-bold text-2xl mb-6">MENU</h2>
                 </div>
                 <ul className="font-Roboto text-purple-600 item-center">
