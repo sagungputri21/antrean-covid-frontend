f0d93dd6dcfa0d189192aa52ae497efc84dd59e0
sagungputri21 <93807864+sagungputri21@users.noreply.github.com>
2021-12-14 08:27:14 +0800

    Update PasienNavLink.js

--- components/Sidebar/PasienNavLink.js
+++ components/Sidebar/PasienNavLink.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import useRouter from "next/router"
+import Link from 'next/link'
+import { useRouter } from "next/router"
 
 const PasienNavLink = ({routes, image, text}) => {
     const router = useRouter()
@@ -8,7 +9,7 @@ const PasienNavLink = ({routes, image, text}) => {
         <li className="mt-4">
             <Link href={routes}>
                     <a className={`font-semibold flex item-center text-lg px-6 py-3 ml-2 mr-2 ${router.pathname === routes ? "text-white bg-purple-400" : ""} hover:bg-purple-400 rounded-lg hover:text-white`}>
-                        <img src={image} alt="home icon" className="h-7 mr-3 hover:stroke-white" />
+                        <img src={image} alt="home icon" className="h-7 mr-3 hover:fill-white" />
                         {text}
                     </a>
             </Link>
