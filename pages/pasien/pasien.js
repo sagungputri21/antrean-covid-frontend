1479709e124c61ae6e919d92763bad95e5c7d30d
sagungputri21 <93807864+sagungputri21@users.noreply.github.com>
2021-12-16 19:06:04 +0800

    Update pasien.js

--- pages/pasien/pasien.js
+++ pages/pasien/pasien.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
 import PasienNav from '../../components/Sidebar/PasienNav'
+import Halo from '../../components/haloNama'
+import Note from '../../components/Custom/note'
 
-const Pasien = () => {
+const pasien = () => {
+
     return (
-        <Layout>
-            <PasienNav>
-
-            </PasienNav>
-        </Layout>
+        <body className="bg-purple-100">
+            <Layout>
+                <div className="flex row">
+                    <PasienNav />
+                    <section className="w-9/12">
+                        <div className="w-full text-center">
+                            <Halo nama="Nama" />
+                        </div>
+                        <div className="mt-40">
+                        <Note desc="Silakan pilih menu yang tersedia pada panel sebelah kiri 
+                        untuk melakukan pendaftaran vaksinasi dan mengecek status vaksinasi Anda. Terima kasih"/>
+                        </div>
+                    </section>
+                </div>
+            </Layout>
+        </body>
     )
 }
 
-export default Pasien
+
+export default pasien
